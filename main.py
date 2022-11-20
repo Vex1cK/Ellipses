6ad377f0bd97e544b11c3323a96c58e36b123456
Vex1cK <118670080+Vex1cK@users.noreply.github.com>
2022-11-20 19:11:18 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 from random import choice
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5 import uic
+from PyQt5 import QtCore, QtWidgets
 import sys
 
-class EllipseWindow(QMainWindow):
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.button = QtWidgets.QPushButton(self.centralwidget)
+        self.button.setGeometry(QtCore.QRect(280, 230, 201, 71))
+        self.button.setObjectName("button")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.button.setText(_translate("MainWindow", "Нажми =3"))
+
+
+class EllipseWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.button.clicked.connect(self.ellipse)
         self.Xes = list(range(600))
         self.Yes = list(range(400))
@@ -20,16 +48,19 @@ class EllipseWindow(QMainWindow):
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
-        qp.setBrush(QColor(255, 255, 0))
+        color = [choice(range(255)) for _ in range(3)]
+        qp.setBrush(QColor(*color))
         a = [choice(i) for i in self.values]
         qp.drawEllipse(a[0], a[1], a[2], a[2])
         qp.end()
+
 
 def main():
     app = QApplication(sys.argv)
     window = EllipseWindow()
     window.show()
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
